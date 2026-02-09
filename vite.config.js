d92421e42923869d51ed0ad0423b08e517dfa1f1
Nina <sakinaismail1918@gmail.com>
2026-02-08 23:32:58 -0500

    render changes

--- vite.config.js
+++ vite.config.js
@@ -32,9 +32,13 @@
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
   base: '/WeddingCard/',
   server: {
     host: true,
